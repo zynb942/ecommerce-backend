ca41c54b5f1d8e0b86170ff81225a20d6f496b3f
unknown <elamin2006@gmail.com>
2026-07-06 18:01:14 +0300

    feat: implement simplified global errorHandler middleware

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,10 @@
+const errorHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  
+  return res.status(statusCode).json({
+    success: false,
+    message: err.message || "Internal server error",
+  });
+};
+
+module.exports = errorHandler;
