82ae41c2c9fe784312c507003521e5ca020dfc11
unknown <elamin2006@gmail.com>
2026-07-11 19:15:23 +0300

    config: add email configuration variables to env.js

--- src/config/env.js
+++ src/config/env.js
@@ -14,6 +14,10 @@ const _config = {
   PORT: parseInt(process.env.PORT) || 5000,
   NODE_ENV: process.env.NODE_ENV,
   MONGO_URL: process.env.MONGO_URI,
+  EMAIL_HOST: process.env.EMAIL_HOST || "smtp.gmail.com",
+  EMAIL_PORT: parseInt(process.env.EMAIL_PORT) || 587,
+  EMAIL_USER: process.env.EMAIL_USER, 
+  EMAIL_PASS: process.env.EMAIL_PASS  
 }
 
 module.exports = _config
