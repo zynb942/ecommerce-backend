8ad57a1ad9e25fc5d417e2d93c90d73d8d73bda7
unknown <elamin2006@gmail.com>
2026-07-11 21:57:12 +0300

    refactor: optimize email transporter creation and implement input destructuring with try-catch

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,30 +1,30 @@
 const nodemailer = require("nodemailer");
 const _config = require("../config/env");
 
+const transporter = nodemailer.createTransport({
+  host: _config.EMAIL_HOST,
+  port: _config.EMAIL_PORT,
+  secure: _config.EMAIL_PORT === 465,
+  auth: {
+    user: _config.EMAIL_USER,
+    pass: _config.EMAIL_PASS,
+  },
+});
+
 // Function to send email
-const sendEmail = async (options) => {
+const sendEmail = async ({ to, subject, html }) => {
 
-    const transporter = nodemailer.createTransport({
-    host: _config.EMAIL_HOST,
-    port: _config.EMAIL_PORT,
-    secure: _config.EMAIL_PORT === 465, 
-    auth: {
-      user: _config.EMAIL_USER,
-      pass: _config.EMAIL_PASS,
-    },
-  });
-
-  // Email content
-  const mailOptions = {
-    from: `"E-Commerce Platform" <${_config.EMAIL_USER}>`,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html: options.html 
-  };
-
-  // Send the email
-  await transporter.sendMail(mailOptions);
+try {
+    await transporter.sendMail({
+      from: `"E-Commerce Platform" <${_config.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+  } catch (error) {
+    throw error;
+  }
+  
 };
 
 module.exports = sendEmail;
