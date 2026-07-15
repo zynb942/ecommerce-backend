6b0f4e0dbbb5042b4692011eabf45b3fd489b681
Ali Shaban <alishaaban6008@example.com>
2026-07-15 16:25:06 +0300

    Add user management endpoints - Task 2 (Add User)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authRoutes = require('./routes/auth.routes.js')
+const userRoutes = require('./routes/user.routes.js');
 const errorHandler = require('./middlewares/errorHandler.js')
 
 const app = express();
@@ -7,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/auth', authRoutes)
-
+app.use('/api/users', userRoutes);
 app.use(errorHandler);
 
 module.exports = app;
