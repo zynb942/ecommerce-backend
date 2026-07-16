556386f76fe3e54b1cbab8039e107274cfc0e99c
Mohammad Bilal <mhd.mhdb2.2017@gmail.com>
2026-07-16 03:31:45 +0300

    makiing Delete route and function

--- src/validation/user.validation.js
+++ src/validation/user.validation.js
@@ -1,36 +1,25 @@
 const Joi = require("joi");
 
-
 const addUserSchema = Joi.object({
-  username: Joi.string()
-    .trim()
-    .min(3)
-    .max(30)
-    .required()
-    .messages({
-      "string.base": "Username must be a string",
-      "string.empty": "Username is required",
-      "string.min": "Username must be at least 3 characters",
-      "string.max": "Username must not exceed 30 characters",
-      "any.required": "Username is required",
-    }),
+  username: Joi.string().trim().min(3).max(30).required().messages({
+    "string.base": "Username must be a string",
+    "string.empty": "Username is required",
+    "string.min": "Username must be at least 3 characters",
+    "string.max": "Username must not exceed 30 characters",
+    "any.required": "Username is required",
+  }),
 
-  email: Joi.string()
-    .trim()
-    .lowercase()
-    .email()
-    .required()
-    .messages({
-      "string.base": "Email must be a string",
-      "string.empty": "Email is required",
-      "string.email": "Email must be a valid email",
-      "any.required": "Email is required",
-    }),
+  email: Joi.string().trim().lowercase().email().required().messages({
+    "string.base": "Email must be a string",
+    "string.empty": "Email is required",
+    "string.email": "Email must be a valid email",
+    "any.required": "Email is required",
+  }),
 
   password: Joi.string()
-  .trim()  
-  .pattern(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.#_-])[A-Za-z\d@$!%*?&.#_-]{8,}$/
+    .trim()
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.#_-])[A-Za-z\d@$!%*?&.#_-]{8,}$/,
     )
     .required()
     .messages({
@@ -43,12 +32,13 @@ const addUserSchema = Joi.object({
 
   phone: Joi.string()
     .trim()
-    .pattern(/^\+?[1-9]\d{7,14}$/) 
+    .pattern(/^\+?[1-9]\d{7,14}$/)
     .required()
     .messages({
       "string.base": "Phone must be a string",
       "string.empty": "Phone is required",
-      "string.pattern.base": "Phone must be a valid international number (e.g., +201234567890 or 01234567890)",
+      "string.pattern.base":
+        "Phone must be a valid international number (e.g., +201234567890 or 01234567890)",
       "any.required": "Phone is required",
     }),
 }).options({
@@ -56,30 +46,41 @@ const addUserSchema = Joi.object({
   allowUnknown: false,
 });
 
-
-
 const updateUserSchema = Joi.object({
   username: Joi.string().trim().min(3).max(30).messages({
     "string.base": "Username must be a string",
     "string.min": "Username must be at least 3 characters long",
     "sring.max": "Username cannot exceed 30 characters",
   }),
-  phone: Joi.string().pattern(/^\+?[1-9]\d{7,14}$/).messages({
-    "string.base": "Phone must be a string",
-    "string.pattern.base": "Phone number must be a valid international format",
-  }),
+  phone: Joi.string()
+    .pattern(/^\+?[1-9]\d{7,14}$/)
+    .messages({
+      "string.base": "Phone must be a string",
+      "string.pattern.base":
+        "Phone number must be a valid international format",
+    }),
 
   avatar: Joi.string().trim().uri().messages({
-   "string.base": "Avatar must be a string",
+    "string.base": "Avatar must be a string",
     "string.uri": "Avatar must be a valid URL",
   }),
+});
+
+const userIdSchema = Joi.object({
+  id: Joi.string().hex().length(24).required().messages({
+    "string.base": "User ID must be a string",
+    "string.hex": "User ID must be a valid hexadecimal string",
+    "string.length": "User ID must be exactly 24 characters long",
+    "any.required": "User ID is required",
+  }),
 })
-.options({
+
+  .options({
     abortEarly: false,
     allowUnknown: false,
-
   });
 module.exports = {
   updateUserSchema,
   addUserSchema,
+  userIdSchema,
 };
