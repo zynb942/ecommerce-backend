6816222210b3593892462e59ea67791d94345061
Ali Shaban <alishaaban6008@example.com>
2026-07-23 04:47:19 +0300

    update remove product from wishlist functionality (Task 2)

--- src/controllers/wishlist.controller.js
+++ src/controllers/wishlist.controller.js
@@ -12,30 +12,24 @@ const removeFromWishlist = asyncHandler(async (req, res) => {
   const { productId } = req.params;
   const userId = req.user._id;
 
-  //  Find the wishlist for the current user
   const wishlist = await Wishlist.findOne({ user: userId });
 
-  //  If no wishlist exists, return 404
   if (!wishlist) {
     throw new ApiError(404, "Wishlist not found");
   }
 
-  //  Check if the product exists in the wishlist
+  
   const productIndex = wishlist.products.findIndex(
     (product) => product._id.toString() === productId
   );
 
-  //  If product is not found, return 404
   if (productIndex === -1) {
     throw new ApiError(404, "Product not found in wishlist");
   }
 
-  //  Remove the product from the wishlist
   wishlist.products.splice(productIndex, 1);
-
-  //  Save the updated wishlist
   await wishlist.save();
-  //  Return success response
+  await wishlist.populate("products");
   return sendResponse(
     res,
     200,
